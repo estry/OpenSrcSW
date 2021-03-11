7723b2a9384230d48ad77a26c07281619bc2b7f5
kiyongname <stellis001@gmail.com>
2021-03-11 13:04:12 +0900

    new file:   .gitignore 	new file:   data/collection.xml 	new file:   "html_data/\353\226\241.html" 	new file:   "html_data/\353\235\274\353\251\264.html" 	new file:   "html_data/\354\225\204\354\235\264\354\212\244\355\201\254\353\246\274.html" 	new file:   "html_data/\354\264\210\353\260\245.html" 	new file:   "html_data/\355\214\214\354\212\244\355\203\200.html" 	new file:   src/HtmlReader.java 	new file:   src/Main.java 	new file:   src/MakeXml.java

---
+++ src/HtmlReader.java
@@ -0,0 +1,50 @@
+import java.io.*;
+
+public class HtmlReader {
+    private MakeXml makeXml;
+
+    public HtmlReader() {
+    }
+
+    public HtmlReader(MakeXml makeXml) {
+        this.makeXml = makeXml;
+    }
+
+    public void readHtml() {
+        File dir = new File("./html_data");
+
+        File[] fileList = dir.listFiles();
+
+        for (int i = 0; i < 1; i++) {
+            try {
+                FileReader fileReader = new FileReader(fileList[i]);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    //xml 생성
+                    //0==doc_id, 1 == title, 2 == body
+                    if (line.contains("<head>")) {
+                        this.makeXml.writeXml(0, Integer.toString(i));
+                    }
+                    if (line.contains("<title>")) {
+                        line.replace("<title>"," ");
+                        line.replace("</title>"," ");
+                        this.makeXml.writeXml(1,line);
+                    } else if (line.contains("<div id=\"content\">")) {
+                        line.replace("<p>"," ");
+                        line.replace("</p>"," ");
+                        this.makeXml.writeXml(2,line);
+                    }
+                }
+                bufferedReader.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+}
