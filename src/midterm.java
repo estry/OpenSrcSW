16e45075e7bb87c105e3b0fb76d0f483233adbbf
kiyongname <stellis001@gmail.com>
2021-04-20 18:55:17 +0900

    오픈소스 소프트웨어 입문 중간고사 제출

--- src/midterm.java
+++ src/midterm.java
@@ -1,3 +1,7 @@
+// 제출한 url로 접속하시면 master branch 로 연결됩니다.
+// 컴파일 할 때
+// javac -cp ./lib/* *.java -encoding UTF-8
+// 기존 컴파일하는 방식 그대로 이용했습니다.
 import java.io.FileNotFoundException;
 
 public class midterm {
